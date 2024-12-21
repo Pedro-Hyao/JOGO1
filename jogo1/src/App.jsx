f30881e7029634e3295683f877fc450c8a13d442
Pedro-Hyao <pamonha0222@gmail.com>
2024-12-21 05:57:59 +0000

    versao 0.1

---
+++ jogo1/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Box from '@mui/material/Box';
+
+function App() {
+
+  return ( 
+  <Box
+  display="flex"
+  width="100vw"
+  height="100vh"
+  bgcolor="red">
+aaaa
+</Box>
+
+  )
+}
+
+export default App
